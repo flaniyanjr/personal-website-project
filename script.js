f0e03297922f9a9b118cfe13aa2fc7466391884b
Felix Laniyan <flaniyanjr@gmail.com>
2023-09-04 00:53:04 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+
+const paragraph= document.createElement("p")
+paragraph.textContent = "That's what I'm talking about. That's why he's the MVP. That's why he's the goat. THE GOAAAAATT!!"
+document.querySelector('body').appendChild(paragraph)
